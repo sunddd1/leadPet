8c1be744e9482a3cbf126cc8a1cb6616e9c20cc2
DESKTOP-T4SP14R\GDJ31 <gs903100@naver.com>
2021-04-27 15:42:24 +0900

    (UI) 미니게임 메인 페이지 -> 네비바 되면 수정 예정 (UI) 게임 회차 페이지 -> 네비바 되면 수정 예정 (UI) 문제 리스트 페이지 -> 네비바 되면 수정 예정 상식퀴즈 문제 리스트 불러오기 기능

--- src/main/java/com/spring/main/dao/GameDAO.java
+++ src/main/java/com/spring/main/dao/GameDAO.java
@@ -1,5 +1,13 @@
 package com.spring.main.dao;
 
+import java.util.ArrayList;
+
+import com.spring.main.dto.GameDTO;
+
 public interface GameDAO {
 
+	ArrayList<GameDTO> quizList();
+
+	ArrayList<GameDTO> nemoList();
+	
 }
